5db80c1a3552808111f41b2b01648051e969fc05
Bogdan Orzea <bogdan.orzea@gmail.com>
2020-12-29 17:12:35 -0500

    Complete Section 15

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -21,7 +21,6 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.title = K.appName
         navigationItem.hidesBackButton = true
 
         tableView.dataSource = self
@@ -50,6 +49,8 @@ class ChatViewController: UIViewController {
 
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
+                            let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                            self.tableView.scrollToRow(at: indexPath, at: .top, animated: false)
                         }
                     }
                 }
@@ -57,7 +58,7 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
-        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+        if let messageBody = messageTextfield.text, messageBody.count > 1, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
                 K.FStore.bodyField: messageBody,
@@ -66,7 +67,9 @@ class ChatViewController: UIViewController {
                 if let e = error {
                     print("There was an error sending the message: \(e.localizedDescription)")
                 } else {
-                    print("Message was sent")
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
                 }
             }
         }
@@ -88,9 +91,21 @@ extension ChatViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
 
-        cell.label?.text = messages[indexPath.row].body
+        cell.label?.text = message.body
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+            cell.rightImageView.isHidden = false
+        } else {
+            cell.leftImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.rightImageView.isHidden = true
+        }
 
         return cell
     }
